5d2737e6dfeb1cbcb41aea8be2248f42d4cd69be
WiktoriaSmulska <wiktoriasmulska0@gmail.com>
2025-02-14 20:20:35 +0100

    drobne zmiany w testach sr

--- tests/speech_recognizer/test_speech_recognizer.py
+++ tests/speech_recognizer/test_speech_recognizer.py
@@ -1,6 +1,12 @@
 import logging
+import re
+from asyncio import Queue
 from unittest.mock import patch, MagicMock
 import speech_recognition as sr
+
+from src.speech_recognizer import SpeechRecognizer
+from src.speech_recognizer_service import SpeechRecognizerService
+
 logging.basicConfig(level=logging.DEBUG)
 
 @patch("speech_recognition.Recognizer.listen")
@@ -33,33 +39,30 @@ def test_listen_to_microphone_unknown_value2(mock_listen, mock_recognize, speech
     assert "Preparing..." in result.out
     assert "Could not request results from Google Speech Recognition service." in result.out
 
-#TODO czemu na tym teście poniżej program się zawiesza?
-#TODO czy dobrze w tym teście wszystko mockuje czy może powinnam to jakoś inaczej zrobić?
-@patch("speech_recognition.Recognizer.listen")
-@patch("speech_recognition.Recognizer.recognize_google", return_value="numer paczki P123")
-def test_handle_conversation_package(mock_listen, mock_recognize, speech_recognizer, capsys):
-    speech_recognizer.listen_to_microphone = MagicMock(return_value=None)
 
-    speech_recognizer.transcript_queue.get = MagicMock(return_value="numer paczki p123")
 
-    speech_recognizer.eleven_client.generate = MagicMock(return_value=b"Generated audio")
+def test_handle_conversation():
+    mock_service = MagicMock(spec=SpeechRecognizerService)
+    speech_recognizer = SpeechRecognizer(mock_service)
+    listen_mic = speech_recognizer.listen_to_microphone= MagicMock()
 
-    speech_recognizer.eleven_client.play = MagicMock()
+    speech_recognizer.transcript_queue = Queue()
+    speech_recognizer.transcript_queue.put("numer Paczki P12345")
+    #speech_recognizer.transcript_queue.put("koniec")
 
-    speech_recognizer.speech_recognizer_service.find_locker = MagicMock(return_value="Your package P123 is in locker 5")
+    #with patch("builtins.input", side_effect=["koniec"]):
+    speech_recognizer.handle_conversation()
+    transcript_result = speech_recognizer.transcript_queue.get()
+    transcript_lower = transcript_result.strip().lower()
 
-    with patch("builtins.input", side_effect=["numer paczki p123", "zakończ"]):
-        speech_recognizer.handle_conversation()
+    regex_result = test_regex(transcript_lower)
+    assert regex_result == "P12345"
+    listen_mic.assert_called_once()
 
-    speech_recognizer.speech_recognizer_service.find_locker.assert_called_once_with("p123")
+def test_regex(text:str)->str | None:
+    match = re.search(r'(?i)numer\s+paczki\s+(P\d{0,9})', text) or \
+            re.search(r'(?i)znajdź\s+paczkomat\s+dla\s+paczki\s+(P\d{0,9})', text)
 
-    speech_recognizer.eleven_client.generate.assert_called_once_with(
-        text="Your package P123 is in locker 5", voice='Bill'
-    )
+    return match
 
-    #speech_recognizer.eleven_client.play.assert_called_once()
-    generated_audio = speech_recognizer.eleven_client.generate.return_value
-    #logging.debug(f"Generated audio type: {type(generated_audio)}")  # Should be <class 'bytes'>
 
-    captured = capsys.readouterr()
-    assert "Your package P123 is in locker 5" in captured.out
